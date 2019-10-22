c3107850942e6c6f4c3ce0b7c4100c200c0aaf59
Jesús Enrique Rascón <blograso@gmail.com>
2019-10-23 00:01:31 +0200

    Module developed finally!
    The Terrain class is finally implemented and works fine accessing its methods and such.

--- terrainGenerator.py
+++ terrainGenerator.py
@@ -4,6 +4,12 @@ from PIL import Image
 
 
 class Terrain:
+    '''Terrain class with main methods such as a constructor and some helpers like display_world or add_color
+    
+    The class also has some parameters like colors or the seed to speed up the creating process. 
+
+    DO NOT FORGET TO ADD SELF in front of any attribute we may want to refer to.
+    '''
     blue = [65, 105, 225]
     green = [34, 139, 34]
     beach = [240, 233, 175]
@@ -16,8 +22,9 @@ class Terrain:
 
     # functions
     def __init__(self, shape, scale, octaves, persistence, lacunarity):
-        world = self.generateWorld(shape, scale, octaves, persistence,
-                                   lacunarity)
+        self.world = self.normalize(
+            self.generateWorld(shape, scale, octaves, persistence, lacunarity),
+            0, 255)
 
     def normalize(self, oldWorld, newMin, newMax):
         '''Normalizes de input array between newMin and newMax.'''
@@ -33,22 +40,22 @@ class Terrain:
         world = np.zeros(shape)
         for i in range(shape[0]):
             for j in range(shape[1]):
-                world[i][j] = noise.pnoise2(i / scale + 50,
+                world[i][j] = noise.pnoise3(i / scale + 50,
                                             j / scale + 50,
+                                            self.seed,
                                             octaves=octaves,
                                             persistence=persistence,
                                             lacunarity=lacunarity,
                                             repeatx=1024,
                                             repeaty=1024,
                                             base=0)
+        return world
 
-        return self.normalize(world, 0, 255)
-
-    def display_world(world):
+    def display_world(self):
         '''Reproduce the resulting world as an image.'''
-        Image.fromarray(world).show()
+        Image.fromarray(self.world).show()
 
-    def add_color(world):
+    def add_color(self):
         '''Adds color to the input array based on interval values.
         >< 60 = blue \n
         >\>= 60 & < 70 = beach \n
@@ -58,17 +65,19 @@ class Terrain:
         >\>= 190 & < 240 = mountain \n
         >\> 240 = snow
         '''
-        color_world = np.zeros((shape[0], shape[1], 3), 'uint8')
 
-        color_world[world < 60] = blue
-        color_world[(world >= 60) & (world < 70)] = beach
-        color_world[(world >= 70) & (world < 100)] = lightGreen
-        color_world[(world >= 100) & (world < 150)] = green
-        color_world[(world >= 150) & (world < 190)] = darkGreen
-        color_world[(world >= 190) & (world < 240)] = mountain
-        color_world[world > 240] = snow
+        color_world = np.zeros((self.world.shape[0], self.world.shape[1], 3),
+                               'uint8')
 
-        return color_world
+        color_world[self.world < 60] = self.blue
+        color_world[(self.world >= 60) & (self.world < 70)] = self.beach
+        color_world[(self.world >= 70) & (self.world < 100)] = self.lightGreen
+        color_world[(self.world >= 100) & (self.world < 150)] = self.green
+        color_world[(self.world >= 150) & (self.world < 190)] = self.darkGreen
+        color_world[(self.world >= 190) & (self.world < 240)] = self.mountain
+        color_world[self.world > 240] = self.snow
+
+        self.world = color_world
 
 
 if __name__ == "__main__":
@@ -77,9 +86,6 @@ if __name__ == "__main__":
     # octaves = 6
     # persistence = 0.45
     # lacunarity = 2
-    terrain = Terrain(shape=(800, 800),
-                      scale=500.0,
-                      octaves=6,
-                      persistence=0.45,
-                      lacunarity=2)
-    Terrain.display_world(terrain.world)
+    terrain = Terrain((800, 800), 500.0, 6, 0.45, 2)
+    terrain.add_color()
+    terrain.display_world()
