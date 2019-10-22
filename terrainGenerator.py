f9bca12c78c08c3d3cb30a5945209512dca0c0d9
Jesús Enrique Cartas Rascón <jecr0002@red.ujaen.es>
2019-10-22 12:18:50 +0200

    terrain finally being generated!

--- terrainGenerator.py
+++ terrainGenerator.py
@@ -1,52 +1,69 @@
 import noise
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+import sklearn
 
 shape = (1024, 1024)
-scale = 50.0
+scale = 100.0
 octaves = 6
 persistence = 0.5
-lacunarity = 2.0
+lacunarity = 1.37
 
-blue = (65, 105, 225)
-green = (34, 139, 34)
-beach = (238, 214, 175)
+blue = [65, 105, 225]
+green = [34, 139, 34]
+beach = [238, 214, 175]
 
 world = np.zeros(shape)
 
 
+# functions
 def display_world(world):
-    plt.figure(figsize=(10, 10))
-    plt.imshow(world)
+    '''Reproduce the resulting world as an image'''
+    Image.fromarray(world).show()
 
 
 def add_color(world):
-    color_world = np.array(world, np.int32)
+    color_world = np.zeros((shape[0], shape[1], 3), 'uint8')
     for i in range(shape[0]):
         for j in range(shape[1]):
-            if world[i][j] < -0.05:
+            if world[i][j] < 100:
                 color_world[i][j] = blue
-            elif world[i][j] < 0:
+            elif world[i][j] < 120:
                 color_world[i][j] = beach
-            elif world[i][j] < 1.0:
+            elif world[i][j] < 255:
                 color_world[i][j] = green
 
     return color_world
 
 
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        world[i][j] = noise.pnoise2(i / scale,
-                                    j / scale,
-                                    octaves=octaves,
-                                    persistence=persistence,
-                                    lacunarity=lacunarity,
-                                    repeatx=1024,
-                                    repeaty=1024,
-                                    base=0)
+def normalize(oldWorld, newMin, newMax):
+    oldMax = oldWorld.max()
+    oldMin = oldWorld.min()
 
-display_world(world)
+    factor = (newMax - newMin) / (oldMax - oldMin)
+    newWorld = (oldWorld - oldMin) * factor + newMin
+    return newWorld
 
-colored_world = add_color(world)
 
-display_world(colored_world)
+def generateWorld(shape, world, scale, octaves, persistence, lacunarity):
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            world[i][j] = noise.pnoise2(i / scale,
+                                        j / scale,
+                                        octaves=octaves,
+                                        persistence=persistence,
+                                        lacunarity=lacunarity,
+                                        repeatx=1024,
+                                        repeaty=1024,
+                                        base=0)
+
+    return normalize(world, 0, 255)
+
+
+if __name__ == "__main__":
+    world = generateWorld(shape, world, scale, octaves, persistence,
+                          lacunarity)
+    display_world(world)
+    colored_world = add_color(world)
+    display_world(colored_world)
