5d75b1eb6d258a8bbb22a909e949e95339f581c6
Jesús Enrique Cartas Rascón <jecr0002@red.ujaen.es>
2019-10-22 19:22:38 +0200

    Documentation for functions were made. Class implementation still yet to do

--- terrainGenerator.py
+++ terrainGenerator.py
@@ -2,72 +2,84 @@ import noise
 import numpy as np
 from PIL import Image
 
-shape = (1024, 1024)
-scale = 500.0
-octaves = 6
-persistence = 0.45
-lacunarity = 2
 
-blue = [65, 105, 225]
-green = [34, 139, 34]
-beach = [240, 233, 175]
-lightGreen = [119, 204, 65]
-darkGreen = [17, 74, 17]
-snow = [255, 250, 250]
-mountain = [139, 137, 137]
+class Terrain:
+    blue = [65, 105, 225]
+    green = [34, 139, 34]
+    beach = [240, 233, 175]
+    lightGreen = [119, 204, 65]
+    darkGreen = [17, 74, 17]
+    snow = [255, 250, 250]
+    mountain = [139, 137, 137]
+    seed = 234567546
+    world = None
 
-world = np.zeros(shape)
+    # functions
+    def __init__(self, shape, scale, octaves, persistence, lacunarity):
+        world = self.generateWorld(shape, scale, octaves, persistence,
+                                   lacunarity)
 
-seed = 234567546
+    def normalize(self, oldWorld, newMin, newMax):
+        '''Normalizes de input array between newMin and newMax.'''
+        oldMax = oldWorld.max()
+        oldMin = oldWorld.min()
 
+        factor = (newMax - newMin) / (oldMax - oldMin)
+        newWorld = (oldWorld - oldMin) * factor + newMin
+        return newWorld
 
-# functions
-def display_world(world):
-    '''Reproduce the resulting world as an image'''
-    Image.fromarray(world).show()
+    def generateWorld(self, shape, scale, octaves, persistence, lacunarity):
+        '''Generates a numpy array filled with noise values which are then normalized.'''
+        world = np.zeros(shape)
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                world[i][j] = noise.pnoise2(i / scale + 50,
+                                            j / scale + 50,
+                                            octaves=octaves,
+                                            persistence=persistence,
+                                            lacunarity=lacunarity,
+                                            repeatx=1024,
+                                            repeaty=1024,
+                                            base=0)
 
+        return self.normalize(world, 0, 255)
 
-def add_color(world):
-    color_world = np.zeros((shape[0], shape[1], 3), 'uint8')
+    def display_world(world):
+        '''Reproduce the resulting world as an image.'''
+        Image.fromarray(world).show()
 
-    color_world[world < 60] = blue
-    color_world[(world >= 60) & (world < 70)] = beach
-    color_world[(world >= 70) & (world < 100)] = lightGreen
-    color_world[(world >= 100) & (world < 150)] = green
-    color_world[(world >= 150) & (world < 190)] = darkGreen
-    color_world[(world >= 190) & (world < 240)] = mountain
-    color_world[world > 240] = snow
+    def add_color(world):
+        '''Adds color to the input array based on interval values.
+        >< 60 = blue \n
+        >\>= 60 & < 70 = beach \n
+        >\>= 70 & < 100 = lightGreen \n
+        >\>= 100 & < 150 = green \n
+        >\>= 150 & < 190 = darkGreen \n
+        >\>= 190 & < 240 = mountain \n
+        >\> 240 = snow
+        '''
+        color_world = np.zeros((shape[0], shape[1], 3), 'uint8')
 
-    return color_world
+        color_world[world < 60] = blue
+        color_world[(world >= 60) & (world < 70)] = beach
+        color_world[(world >= 70) & (world < 100)] = lightGreen
+        color_world[(world >= 100) & (world < 150)] = green
+        color_world[(world >= 150) & (world < 190)] = darkGreen
+        color_world[(world >= 190) & (world < 240)] = mountain
+        color_world[world > 240] = snow
 
-
-def normalize(oldWorld, newMin, newMax):
-    oldMax = oldWorld.max()
-    oldMin = oldWorld.min()
-
-    factor = (newMax - newMin) / (oldMax - oldMin)
-    newWorld = (oldWorld - oldMin) * factor + newMin
-    return newWorld
-
-
-def generateWorld(shape, world, scale, octaves, persistence, lacunarity):
-    for i in range(shape[0]):
-        for j in range(shape[1]):
-            world[i][j] = noise.pnoise2(i / scale + 50,
-                                        j / scale + 50,
-                                        octaves=octaves,
-                                        persistence=persistence,
-                                        lacunarity=lacunarity,
-                                        repeatx=1024,
-                                        repeaty=1024,
-                                        base=0)
-
-    return normalize(world, 0, 255)
+        return color_world
 
 
 if __name__ == "__main__":
-    world = generateWorld(shape, world, scale, octaves, persistence,
-                          lacunarity)
-    # display_world(world)
-    colored_world = add_color(world)
-    display_world(colored_world)
+    # shape = (1024, 1024)
+    # scale = 500.0
+    # octaves = 6
+    # persistence = 0.45
+    # lacunarity = 2
+    terrain = Terrain(shape=(800, 800),
+                      scale=500.0,
+                      octaves=6,
+                      persistence=0.45,
+                      lacunarity=2)
+    Terrain.display_world(terrain.world)
