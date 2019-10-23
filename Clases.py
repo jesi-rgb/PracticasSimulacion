d8fd6d17cc5f6e5c84587d7acb876dc9d0109c8d
Alejandro de la Cruz <alxar98@gmail.com>
2019-10-23 19:13:16 +0200

    Cambiado fichero TerrainGenerator.py

--- Clases.py
+++ Clases.py
@@ -1,25 +1,22 @@
 class Animal:
+    def __init__(self):
 
-	def __init__(self):
         #Survival attributes
         self.hunger = 0
-		self.strength_speed = 0
-		self.reproductive_need = 0
-		self.vision_field = 0
-		self.risk_aversion = 0
-		self.racionality = 0
-        
+        self.strength_speed = 0
+        self.reproductive_need = 0
+        self.vision_field = 0
+        self.risk_aversion = 0
+        self.racionality = 0
+
         #Life attributes
-		self.max_time_alive = 100		
-		self.time_alive = 0
-        
+        self.max_time_alive = 100
+        self.time_alive = 0
+
         #Map position
         self.x = 0
         self.y = 0
-        
-    def nextAction(self):
-        hunger += 0.01
-        
+
         
 class Rabbit(Animal):
     
